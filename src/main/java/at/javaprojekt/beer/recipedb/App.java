bbb9332c61e577a1d5522c9819710d3909ec848d
christopher123 <beerli2@gmx.at>
2013-05-08 16:04:57 +0200

    Initial Project

---
+++ src/main/java/at/javaprojekt/beer/recipedb/App.java
@@ -0,0 +1,16 @@
+package at.javaprojekt.beer.recipedb;
+
+import at.javaprojekt.beer.recipedb.gui.MainFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        MainFrame window = new MainFrame();
+        window.show();
+    }
+}
